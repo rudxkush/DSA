8bde2a42f8bd395d9030ee5277a5a282b9955f4b
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-10-07 01:55:51 +0530

    Update Binary Tree Preorder Traversal - LeetCode (Recursive + Iterative).cpp

--- Binary Tree/Binary Tree Preorder Traversal - LeetCode (Recursive + Iterative).cpp
+++ Binary Tree/Binary Tree Preorder Traversal - LeetCode (Recursive + Iterative).cpp
@@ -1,1 +1,23 @@
-// need to write it asap
+void iterativePreorder(TreeNode* root) {
+  if(root == NULL) return ;  
+  stack<TreeNode*> st;
+  st.push(root);
+  
+  while(!st.empty()) {
+    TreeNode* curr = st.top(); st.pop();
+    cout << curr->val << " ";
+
+    if(curr->right != NULL) st.push(curr->right);
+    if(curr->left != NULL) st.push(curr->left);
+  }
+  return ;
+}
+
+void recursivePostorder(TreeNode* root) {
+  if(root == NULL) {
+    return ;
+  }
+  cout << root->val << " ";
+  recursiveInorder(root->left);
+  recursiveInorder(root->right);
+}
