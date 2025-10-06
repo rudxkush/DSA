45f9b18cb442612896f7ea7f98945924a3bea9bc
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-10-07 00:01:45 +0530

    Update Binary Tree Inorder Traversal - LeetCode (Recursive + Iterative).cpp

--- Binary Tree/Binary Tree Inorder Traversal - LeetCode (Recursive + Iterative).cpp
+++ Binary Tree/Binary Tree Inorder Traversal - LeetCode (Recursive + Iterative).cpp
@@ -1,1 +1,31 @@
-// asap
+void getAllLeft(stack<TreeNode*>& st, TreeNode* node) {
+  while(node != NULL) {
+    st.push(node);
+    node = node->left;
+  }
+}
+
+void iterativeInorder(TreeNode* root) {
+  stack<TreeNode*> st;
+  getAllLeft(st, root);
+
+  while(!st.empty()) {
+    TreeNode* curr = st.top(); 
+    st.pop();
+
+    cout << curr->val << " ";
+
+    if(curr->right != NULL) {
+      getAllLeft(st, curr->right);
+    }
+  }
+}
+
+void recursiveInorder(TreeNode* root) {
+  if(root == NULL) {
+    return ;
+  }
+  recursiveInorder(root->left);
+  cout << root->val << " ";
+  recursiveInorder(root->right);
+}
