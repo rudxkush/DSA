93c56b41781df1b5e2667e75a35fa03928f58a79
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-10-15 04:51:11 +0530

    Update Missing Number.cpp

--- Bit Manipulation/Missing Number.cpp
+++ Bit Manipulation/Missing Number.cpp
@@ -16,3 +16,15 @@ public:
         return n;
     }
 };
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int totalSum = n * (n + 1) / 2;  
+        int sum = 0;
+        for (int it : nums) {
+            sum += it;
+        }
+        return totalSum - sum;
+    }
+};
