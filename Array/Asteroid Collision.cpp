e345eb08a20b6b743a73ffbb72104a41e2ad9d95
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-10-18 00:22:03 +0530

    Create Asteroid Collision.cpp

---
+++ Array/Asteroid Collision.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& ast) {
+        int n = ast.size();
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            if (ast[i] > 0)
+                st.push(ast[i]);
+            else {
+                int left = -ast[i];
+                while (!st.empty() && st.top() < left && st.top() > 0) {
+                    st.pop();
+                }
+                if (!st.empty() && st.top() == left) {
+                    st.pop();
+                } else if (st.empty() || st.top() < 0) {
+                    st.push(ast[i]);
+                }
+            }
+        }
+        int survived = st.size() - 1;
+        vector<int> astW(survived + 1);
+        while (!st.empty()) {
+            astW[survived] = st.top();
+            survived--;
+            st.pop();
+        }
+        return astW;
+    }
+};
