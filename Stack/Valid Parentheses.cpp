055f604e0b7acbe31b5aa779497af64418fb4554
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-10-18 00:34:00 +0530

    Create Valid Parentheses.cpp

---
+++ Stack/Valid Parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkValidity(char open, char close) {
+        return (open == '(' && close == ')') || (open == '{' && close == '}') ||
+               (open == '[' && close == ']');
+    }
+
+    bool isValid(string s) {
+        stack<char> st;
+        for (char bracket : s) {
+            if (bracket == '(' || bracket == '{' || bracket == '[') {
+                st.push(bracket);
+            } else {
+                if (st.empty())
+                    return false;
+                char top = st.top();
+                st.pop();
+                if (!checkValidity(top, bracket))
+                    return false;
+            }
+        }
+        return st.empty();
+    }
+};
