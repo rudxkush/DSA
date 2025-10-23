6805cf73db23c645e5bb359c8357c78dbb0e9e51
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-10-23 20:41:00 +0530

    Update Fibonacci Number.cpp

--- Recursion/Fibonacci Number.cpp
+++ Recursion/Fibonacci Number.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
-    int fib(int n) {
-        // BASE CONDITION
-        if(n < 2) {
+    vector<int> dp;
+    int rec(int n) {
+        if (n < 2)
             return n;
-        }
 
-        return fib(n - 1) + fib(n - 2);
+        if (dp[n] != -1)
+            return dp[n];
+
+        return dp[n] = rec(n - 1) + rec(n - 2);
+    }
+    int fib(int n) {
+        dp.clear();
+        dp.resize(n + 1, -1);
+        return rec(n);
     }
 };
