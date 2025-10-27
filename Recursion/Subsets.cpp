2010b8dcc8d2533647c514dbeccdfcc04996c0e3
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-10-28 02:15:34 +0530

    Create Subsets.cpp

---
+++ Recursion/Subsets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> allSubsets;
+    void rec(vector<int>& nums, int i, vector<int>& temp) {
+        if (i == nums.size()) {
+            allSubsets.push_back(temp);
+            return;
+        }
+
+        temp.push_back(nums[i]);
+        rec(nums, i + 1, temp);
+        temp.pop_back();
+
+        rec(nums, i + 1, temp);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;
+        rec(nums, 0, temp);
+        return allSubsets;
+    }
+};
