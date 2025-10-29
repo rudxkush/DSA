d4e9f4b77826b841f3ff8be836a0a11ca57a4a69
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-10-29 05:47:22 +0530

    Update N-Queens.cpp

--- Recursion/N-Queens.cpp
+++ Recursion/N-Queens.cpp
@@ -1,40 +1,51 @@
+/*
+    Author: rudxkush
+*/
 class Solution {
 public:
-    vector<vector<string>> res;
-    bool isValid(vector<string>& query, int row, int col, char choice) {
-        // row & column check
-        for(int i = 0; i < query.size(); i++) {
-            if (query[i][col] == 'Q') return false;
-     	    if (query[row][i] == 'Q') return false;
+    int rows = 0, cols = 0;
+    vector<vector<string>> allConfigs;
+    bool puttingHereMakesNoDifference(vector<string>& board, int rank, int file) {
+        // row and column check
+        for(int i = 0; i < board.size(); i++) {
+            if(board[rank][i] == 'Q') return false;
+            if(board[i][file] == 'Q') return false;
         }
+
         // diagonal check
-        for(int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-            if(query[i][j] == 'Q') return false;
+        for(int i = rank, j = file; i >= 0 && j >= 0; i--, j--) {
+            if(board[i][j] == 'Q') return false;
         }
-        // anti-diagonal check
-        for(int i = row, j = col; i >=0 && j < query.size(); i--, j++) {
-            if(query[i][j] == 'Q') return false;
-        }
+
+        // anti diagonal check
+        for(int i = rank, j = file; i >= 0 && j < board.size(); i--, j++) {
+            if(board[i][j] == 'Q') return false;
+        } 
+
         return true;
     }
-    void solve(vector<string>& query, int row, int n_queens) {
-        // Base condition
-        if(row == query.size()) {
-            res.push_back(query);
-     	    return ;
+    void placeNQueens(vector<string>& board, int rank, int n) {
+        // Base Condition
+        if (rank == rows) {
+            allConfigs.push_back(board);
+            return ;
         }
-      
-        for(int col = 0; col < query[row].length(); col++) {
-            if(isValid(query , row, col, 'Q')){
-                query[row][col] = 'Q';
-     	        solve(query, row + 1, n_queens - 1); 
-     	        query[row][col] = '.'; 
-            }  
+
+        // Choices
+        for (int file = 0; file < cols; file++) {
+            if (puttingHereMakesNoDifference(board, rank, file)) {
+                board[rank][file] = 'Q';
+                placeNQueens(board, rank + 1, n - 1);
+                board[rank][file] = '.';
+            }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<string> query(n, string(n, '.'));
-        solve(query, 0, n);
-        return res;
+        // Classic Backracking Problem
+        rows = n;
+        cols = n;
+        vector<string> board(n, string(n, '.'));
+        placeNQueens(board, 0, n);
+        return allConfigs;
     }
 };
