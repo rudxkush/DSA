0cce8077aa5fdcf7a34bb57742923a4991b85555
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-11-03 06:27:18 +0530

    Update Find All Lonely Numbers in the Array.cpp

--- Array/Find All Lonely Numbers in the Array.cpp
+++ Array/Find All Lonely Numbers in the Array.cpp
@@ -1,12 +1,15 @@
+/*
+    Author : rudxkush
+*/
 #include <iostream>
 #include <vector>
 #include <climits>
 #include <unordered_map>
 using namespace std;
 
-class Solution {
+class Solution1 {
 public:
-    vector<int> findLonely(vector<int>& nums) {
+    void findLonely(vector<int>& nums) {
         sort(nums.begin(), nums.end()); // To check (nums[i - 1] + 1 == nums[i] && nums[i + 1] == nums[i]) in O(1)
         int n = nums.size();
         vector<int> lonely;
@@ -17,10 +20,33 @@ public:
                 lonely.push_back(nums[i]);
             }
         }
-        // for (int lone : lonely) {
-        //     cout << lone << " ";
-        // }
-        return lonely;
+        for (int lone : lonely) {
+            cout << lone << " ";
+        }
+        cout << endl;
+    }
+};
+
+class Solution2 {
+public:
+    void findLonely(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for (int num : nums) {
+            mp[num]++;
+        }
+        int n = nums.size();
+        vector<int> lonely;
+        for (int i = 0; i < n; i++) {
+            if (mp[nums[i]] == 1 &&
+                mp.find(nums[i] - 1) == mp.end() &&
+                mp.find(nums[i] + 1) == mp.end()) {
+                lonely.push_back(nums[i]);
+            }
+        }
+        for (int lone : lonely) {
+            cout << lone << " ";
+        }
+        cout << endl;
     }
 };
 
@@ -29,7 +55,11 @@ int main() {
     cin >> n;
     vector<int> nums(n);
     for (int i = 0; i < n; i++) cin >> nums[i];
-    Solution obj;
-    obj.findLonely(nums);
+    cout << "Using Sort -------> TC: O(nlogn), SC: O(0)" << endl;
+    Solution1 obj1;
+    obj1.findLonely(nums);
+    cout << "Using Unordered Map -------> TC: O(n), SC: O(n)" << endl;
+    Solution2 obj2;
+    obj2.findLonely(nums);
     return 0;
 }
