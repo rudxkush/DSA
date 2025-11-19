9b85b20b512c1669995a69a0dd849b758a74c358
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-11-19 23:30:02 +0530

    Update Flip.cpp

--- Array/Flip.cpp
+++ Array/Flip.cpp
@@ -1,3 +1,6 @@
+/*
+    Author : rudxkush
+*/
 vector<int> Solution::flip(string A) {
     int n = A.size();
     int currSum = 0;
@@ -6,20 +9,19 @@ vector<int> Solution::flip(string A) {
     int left = 0;
     int bestLeft = -1, bestRight = -1;
 
-    for (int i = 0; i < n; i++) {
-        // convert 0 -> +1, 1 -> -1
-        int val = (A[i] == '0') ? 1 : -1;
-        currSum += val;
-
-        if (currSum < 0) {
+    for (int right = 0; right < n; right++) {
+        int contribution = (A[right] == '0') ? 1 : -1;
+        currSum += contribution;
+        
+        if(currSum < 0) {
             currSum = 0;
-            left = i + 1;    // reset window
+            left = right + 1;
         } else {
-            if (currSum > maxSum) {
+            if(currSum > maxSum) {
                 maxSum = currSum;
                 bestLeft = left;
-                bestRight = i;
-            }
+                bestRight = right;
+            }   
         }
     }
 
