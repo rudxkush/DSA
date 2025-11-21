c712d62f9a38f5c513e3044f63d40edd0a6aef91
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-11-21 23:18:21 +0530

    Create Total Moves For Bishop!.cpp

---
+++ Math/Total Moves For Bishop!.cpp
@@ -0,0 +1,18 @@
+int Solution::solve(int A, int B) {
+    // Count top left squares
+    int topLeft = min(A, B) - 1;
+
+    // Count bottom right squares
+    int bottomRight = 8 - max(A, B);
+
+    // Count top right squares
+    int topRight = min(A, 9 - B) - 1;
+
+    // Count bottom left squares
+    int bottomLeft = 8 - max(A, 9 - B);
+
+    // Return total count
+    return (topLeft + topRight + bottomRight + bottomLeft);
+}
+
+
