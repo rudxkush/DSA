798473704720532259199916cf8fbef07fc596f0
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-11-22 21:35:37 +0530

    Create Move Zeroes.cpp

---
+++ Array/Move Zeroes.cpp
@@ -0,0 +1,13 @@
+vector<int> Solution::solve(vector<int> &nums) {
+    int j = 0;
+    // move all the nonzero elements advance
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] != 0) {
+            nums[j++] = nums[i];
+        }
+    }
+    for (;j < nums.size(); j++) {
+        nums[j] = 0;
+    }
+    return nums;
+}
