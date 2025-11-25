8731d663e068af24bd24a605993a457b6af5ffd0
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-11-25 06:33:52 +0530

    Create Kth Row of Pascal's Triangle.cpp

---
+++ Array/Kth Row of Pascal's Triangle.cpp
@@ -0,0 +1,38 @@
+/*
+  Author : rudxkush
+*/
+vector<int> Solution::getRow(int A) {
+    vector<int> pascalKthRow(A + 1);
+    pascalKthRow[0] = 1;
+    pascalKthRow[A] = 1;
+    long long temp = 1;
+    for(int i = 1; i < A; i++) {
+        temp *= (A - i + 1);
+        temp /= i;
+        pascalKthRow[i] = temp; // 3
+    }
+    return pascalKthRow;
+}
+/*
+      1
+     1 1
+    1 2 1
+   1 3 3 1 <------ A = 3
+  1 4 6 4 1 <----- A = 4
+  pascalKthRow[0] = 1
+  pascalKthRow[1] = 1
+  1 2 3
+  4 6 4 <----- 4
+  4 * ?/?,  prevComputation * row-col+1/col, 6 * ?/?
+
+  
+  int nCr(int n, int r) {
+      if (r > n) return 0;  
+      long long answer = 1;
+      for(int i = 0; i < r; i++) {
+          answer *= (n - i);
+          answer /= (i + 1);
+      }
+      return answer;
+  }
+*/ 
