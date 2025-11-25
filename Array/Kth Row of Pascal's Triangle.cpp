5516dcdae3bf09770fe8c397f89689b0969d97ce
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-11-25 12:52:12 +0530

    Update Kth Row of Pascal's Triangle.cpp

--- Array/Kth Row of Pascal's Triangle.cpp
+++ Array/Kth Row of Pascal's Triangle.cpp
@@ -14,17 +14,23 @@ vector<int> Solution::getRow(int A) {
     return pascalKthRow;
 }
 /*
-      1
-     1 1
-    1 2 1
-   1 3 3 1 <------ A = 3
-  1 4 6 4 1 <----- A = 4
-  pascalKthRow[0] = 1
-  pascalKthRow[1] = 1
-  1 2 3
-  4 6 4 <----- 4
-  4 * ?/?,  prevComputation * row-col+1/col, 6 * ?/?
+         1
+        1 1
+       1 2 1
+      1 3 3 1 <------ A = 3
+     1 4 6 4 1 <----- A = 4
+    pascalKthRow[0] = 1
+    pascalKthRow[1] = 1
 
+    1 4  6  4 1 <----- A = 4
+   1 5 10 10 5 1<----- A = 5
+   
+   4 * 1          4 * ??                        4 * 1
+   (r-c+1)/c      4*(r-c+1)/c = 4 * 3 / 2 = 6
+   
+   5 * 1                 5 * 2             5 * 2     5 * 1
+   1*(row-col+1)/col     prevComputed*(row - col + 1)/col
+  
   
   int nCr(int n, int r) {
       if (r > n) return 0;  
