ff6d6b5179984b5f016dca867f3b9be3b6d45595
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-11-26 21:24:09 +0530

    Update Sum of pairwise Hamming Distance.cpp

--- Bit Manipulation/Sum of pairwise Hamming Distance.cpp
+++ Bit Manipulation/Sum of pairwise Hamming Distance.cpp
@@ -16,3 +16,23 @@ int bruteSolution::hammingDistance(const vector<int> &A) {
 
     return ans % 1000000007;
 }
+/*
+    Author : rudxkush
+*/
+const int MOD = 1e9+7;
+int optimalSolution::hammingDistance(const vector<int> &A) {
+    int n = A.size();
+    long long ans = 0;
+
+    for(int bitIdx = 0; bitIdx < 32; bitIdx++) {
+        int count_of_ones = 0, count_of_zeroes = 0;
+        for(int i = 0; i < n; i++) {
+            bool isOne = (A[i] & (1 << bitIdx));
+            if(isOne) count_of_ones++;
+            else count_of_zeroes++; 
+        }
+        ans = (ans + (2LL * count_of_ones * count_of_zeroes) % MOD) % MOD;
+    }
+
+    return ans;
+}
