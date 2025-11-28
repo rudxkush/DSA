d2247a6754fec132fa81ff8e4cdb06ec941d5502
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-11-28 20:35:25 +0530

    Update Noble Integer.cpp

--- Array/Noble Integer.cpp
+++ Array/Noble Integer.cpp
@@ -1,14 +1,11 @@
 int Solution::solve(vector<int> &A) {
     int n = (int) A.size();
     sort(A.begin(), A.end());
-    
     for(int i = 0; i < n; i++) {
         if(i < n - 1 && A[i] == A[i + 1]) continue;
         if((n - i - 1) == A[i]) {
             return 1;
         }
     }
-    
-    if(A[n - 1] == 0) return 1;
     return -1;
 }
