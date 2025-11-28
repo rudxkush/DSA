fc7486f5e7957e4caa0d68fbca4c7d37ec34458f
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-11-29 03:40:28 +0530

    Update Rotated Array.cpp

--- Binary Search/Rotated Array.cpp
+++ Binary Search/Rotated Array.cpp
@@ -1,4 +1,4 @@
-int Solution::findMin(const vector<int> &A) {
+int bruteSolution::findMin(const vector<int> &A) {
     int n = (int) A.size();
     for(int i = 1; i < n; i++) {
         if(A[i] < A[i - 1]) {
@@ -6,4 +6,20 @@ int Solution::findMin(const vector<int> &A) {
         }
     }    
     return A[0];
+}
+
+int optimalSolution::findMin(const vector<int> &nums) {
+    int start = 0;
+    int end = nums.size() - 1;
+
+    while (start < end) {
+        int mid = start + (end - start) / 2;
+        if (nums[mid] > nums[end]) {
+            start = mid + 1;
+        } else {
+            end = mid;
+        }
+    }
+
+    return nums[start];  
 }
