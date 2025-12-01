fff6a3ff442feaae5ab001e853cd578cd5148e56
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-12-02 04:15:33 +0530

    Update N by 3 Repeating Number using modified Boyer–Moore.cpp

--- Array/N by 3 Repeating Number using modified Boyer–Moore.cpp
+++ Array/N by 3 Repeating Number using modified Boyer–Moore.cpp
@@ -1,52 +1,41 @@
-int Solution::repeatedNumber(const vector<int> &A) {
-    int n = (int) A.size();
-    if(n == 0) return 0;
+int Solution::repeatedNumber(const vector<int> &nums) {
+    int candidate1 = 0;
+    int candidate2 = 0;
+    int freq1 = 0;
+    int freq2 = 0;
 
-    // using modified Boyer–Moore for > n/3 frequency
-    int c1 = 1, c2 = -1;
-    int v1 = A[0], v2;
-
-    for(int i = 1; i < n; i++) {
-        if(A[i] == v1) {
-            c1++;
-        } 
-        else if(c2 != -1) {
-            if(A[i] == v2) c2++;
-            else {
-                c1--;
-                c2--;
-            }
-        } 
-        else {
-            v2 = A[i];
-            c2 = 1;
-        }
-
-        if(c2 == 0) c2 = -1;
-
-        if(c1 == 0) {
-            if(c2 != -1) {
-                v1 = v2;
-                c1 = c2;
-                c2 = -1;
-            } 
-            else if(i < n - 1) {
-                v1 = A[i + 1];
-                c1 = 1;
-                i++;
-            } 
-            else return -1;
+    // moore voting algorithm -> set 1 then set 2
+    int n = nums.size();
+    for (int i = 0; i < n; i++) {
+        if (candidate1 == nums[i]) {
+            freq1++;
+        } else if (candidate2 == nums[i]) {
+            freq2++;
+        } else if (freq1 == 0) {
+            freq1 = 1;
+            candidate1 = nums[i];
+        } else if (freq2 == 0) {
+            freq2 = 1;
+            candidate2 = nums[i];
+        } else {
+            freq1--, freq2--;
         }
     }
 
-    // verify candidates
-    c1 = 0, c2 = 0;
-    for(int x : A) {
-        if(x == v1) c1++;
-        if(c2 != -1 && x == v2) c2++;
+    // validate both
+    int hasAppeared1 = 0, hasAppeared2 = 0;
+    for (int i = 0; i < n; i++) {
+        if (candidate1 == nums[i]) {
+            hasAppeared1 += 1;
+        } else if (candidate2 == nums[i]) {
+            hasAppeared2 += 1;
+        }
     }
-
-    if(c1 > n / 3.0) return v1;
-    if(c2 > n / 3.0) return v2;
-    return -1;
+    if(hasAppeared1 > n/3) {
+        return candidate1;
+    } else if(hasAppeared2 > n/3) {
+        return candidate2;
+    } else {
+        return -1;
+    }
 }
