8422d14362e6b59187f590113d776f88cc274bd0
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-12-04 10:24:57 +0530

    Update Sorted Permutation Rank.cpp

--- Math/Sorted Permutation Rank.cpp
+++ Math/Sorted Permutation Rank.cpp
@@ -1,27 +1,26 @@
-const int mod = 1000003; 
-int factorial(int num) {
-    int ans = 1; 
-    while (num >= 1) {
-        ans = (ans * num) % mod;
-        num--;
-    }
-    return ans % mod;
-}
+const int mod = 1000003;
 
 int Solution::findRank(string word) {
     int n = (int) word.size();
-    int rank = 0; 
-
+    int rank = 0;
+    // pre-compute the factorial of all the possible permutations
+    vector<long long> fact(n + 1, 1);
+    fact[0] = 1;
+    for(int i = 1; i <= n; i++) {
+        fact[i] =  (fact[i - 1] * i) % mod;
+    }
+    
     for (int i = 0; i < n; i++) {
         int remainingLetters = n - i - 1;
         int countSmallerElements = 0;
 
-        for (int j = i + 1; j < n; j++) { 
+        for (int j = i + 1; j < n; j++) {
             if (word[j] < word[i]) {
                 countSmallerElements++;
             }
         }
-        int contribution = (countSmallerElements * factorial(remainingLetters)) % mod;
+
+        int contribution = (countSmallerElements * fact[remainingLetters]) % mod;
         rank = (rank + contribution) % mod;
     }
     return (rank + 1) % mod;
