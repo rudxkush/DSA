8dfb5f5fe71ef12e24d014dba382ac5de95bbc64
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-12-09 11:40:40 +0530

    Update Find a peak element.cpp

--- Binary Search/Find a peak element.cpp
+++ Binary Search/Find a peak element.cpp
@@ -1,22 +1,17 @@
 int Solution::solve(vector<int> &nums) {
     int n = nums.size();
-    int start = 0, end = n - 1;
-    while(start <= end){
-        int mid = start + (end - start) / 2;
-        if(mid == start && mid == end) return nums[mid];
-        else if(mid == start) {
-            if(nums[mid] > nums[mid + 1]) return nums[mid];
-            else start = mid + 1;
+    int left = 0, right = n - 1, ans = -1;
+    while(left <= right) {
+        int mid = (left + right) >> 1;
+        if((mid == 0 || nums[mid] >= nums[mid - 1]) && (mid == n-1 || nums[mid] >= nums[mid + 1])) {
+            ans = nums[mid]; 
+            break;
         }
-        else if(mid == end) {
-            if(nums[mid] > nums[mid - 1]) return nums[mid];
-            else end = mid - 1;
-        }
-        else {
-            if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return nums[mid];
-            else if(nums[mid] > nums[mid - 1]) start = mid + 1;
-            else end = mid - 1;
+        if((mid == 0 || nums[mid] >= nums[mid-1])) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
-    return -1;
+    return ans;
 }
