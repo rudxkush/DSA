872cf72e3c7d13d4c8702a53df981b591681b4be
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-12-10 20:40:29 +0530

    Update Reverse Bits.cpp

--- Bit Manipulation/Reverse Bits.cpp
+++ Bit Manipulation/Reverse Bits.cpp
@@ -1,8 +1,16 @@
 unsigned int Solution::reverse(unsigned int A) {
-    unsigned int ans = 0;
-    for (int i = 0; i < 32; i++) { // Loop through all 32 bits
-        ans = (ans << 1) | (A & 1); // Shift `ans` left and add the least significant bit of `A`
-        A >>= 1; // Shift `A` right to process the next bit
+    unsigned int num = 0;
+    for(int i = 0; i < 32; i++) {
+        if((A >> i) & 1) {
+            num |= 1 << (31 - i); // need a one here 
+        } 
     }
-    return ans;
+    return num;
 }
+
+/*
+
+    0000 0000 0000 0000 0000 0000 0000 0011 -> 3 (rep in binary)
+    1100 0000 0000 0000 0000 0000 0000 0000 -> 3221225472 (rep in binary)
+    
+*/
