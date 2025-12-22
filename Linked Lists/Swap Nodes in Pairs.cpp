77c5ddfa90e66c0ab1c30ba3019a58ca01c4c704
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-12-22 11:55:14 +0530

    Create Swap Nodes in Pairs.cpp

---
+++ Linked Lists/Swap Nodes in Pairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head == NULL) return head;
+        ListNode* dummy = new ListNode(0); 
+        dummy->next = head;
+        ListNode* iter = dummy;
+        while(iter->next && iter->next->next) {
+            // we can swap the two nodes provided both the
+            // adjacent nodes are not NULL
+            ListNode* prev = iter;
+            ListNode* nextNodes = iter->next->next->next;
+            ListNode* first = iter->next;
+            ListNode* second = iter->next->next;
+            first->next = nextNodes;
+            second->next = first;
+            prev->next = second;
+            iter = first;
+        }
+        return dummy->next;
+    }
+};
